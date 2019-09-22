a068a0091665e287132beb22453a06ac08b60456
Alkalus <3060479+draknyte1@users.noreply.github.com>
2019-09-22 16:42:12 +0100

    Updated Copyright notices on every class.

--- src/main/java/com/sonicether/soundphysics/SPGuiConfig.java
+++ src/main/java/com/sonicether/soundphysics/SPGuiConfig.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ *                     GNU GENERAL PUBLIC LICENSE
+ *                        Version 3, 29 June 2007
+ *
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ *
+ *  https://github.com/sonicether/Sound-Physics/blob/master/LICENSE
+ *
+ * Copyright (c) 2017, 2019 AlkCorp.
+ * Contributors: https://github.com/alkcorp/Sound-Physics/graphs/contributors
+ *******************************************************************************/
 package com.sonicether.soundphysics;
 
 import net.minecraft.client.gui.GuiScreen;
