d7bf29691f8873d04f1d3468127af7fb7946a0d3
Alkalus <3060479+draknyte1@users.noreply.github.com>
2019-09-23 04:03:04 +0100

    More small fixes.

--- src/main/java/net/alkalus/envirosound/VersionLoader.java
+++ src/main/java/net/alkalus/envirosound/VersionLoader.java
@@ -1,14 +1,18 @@
 package net.alkalus.envirosound;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Properties;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
 
 public class VersionLoader {
 
 	private static VersionLoader INSTANCE;
-	private Properties versionProperties = new Properties();
-	private final String version_Internal = getVersion();
-	public final String VERSION = version_Internal;
+	public final String VERSION;
 	
 	public static VersionLoader getInstance() {
 		if (INSTANCE == null) {
@@ -18,15 +22,44 @@ public class VersionLoader {
 	}	
 	
 	private VersionLoader() {
-		try {
-			versionProperties.load(VersionLoader.class.getResourceAsStream("resources/version.properties"));
+		File mcmod = new File(getClass().getClassLoader().getResource("mcmod.info").getFile());		
+		if (mcmod == null || !mcmod.exists()) {
+			URL temp = getClass().getResource("mcmod.info");
+			if (temp != null) {
+				try {
+					FileUtils.copyURLToFile(temp, mcmod);
+				}
+				catch (IOException e) {
+					mcmod = new File("resources/mcmod.info");
+				}
+			}
+			else {
+				mcmod = new File("resources/mcmod.info");
+			}
+		}		
+		String VERSION_TEMP = "no version";
+		try (BufferedReader br = new BufferedReader(new FileReader(mcmod))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		       if (line != null && line.length() > 0) {
+		    	   if (line.contains("\"version\": \"")) {
+		    		   VERSION_TEMP = line;
+		    	   }
+		       }
+		    }
+		}
+		catch (FileNotFoundException e) {
 		}
 		catch (IOException e) {
-			
 		}
+		if (!VERSION_TEMP.equals("no version")) {
+			String temp1 = VERSION_TEMP.replace("", "");
+			temp1 = temp1.replaceAll(" ", "").replace("version", "").replaceAll(":", "").replaceAll("\"", "").replaceAll(",", "");
+			VERSION_TEMP = temp1;
+		}
+		VERSION = VERSION_TEMP;
 	}
 	public String getVersion() {
-		String y = versionProperties.getProperty("version");
-		return y != null ? y : "no version";
+		return VERSION;
 	}
 }
